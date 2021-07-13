668de4ae05aceaec5da7446175a03e42e681567e
Gleason <agleason>
2021-07-13 10:40:54 -0500

    add TX search to navbar

--- src/App.vue
+++ src/App.vue
@@ -4,8 +4,9 @@
     <v-app-bar app style="background-color: #0a0a57">
       <v-app-bar-nav-icon @click="drawerState = !drawerState" style="color: white"></v-app-bar-nav-icon>
       <v-toolbar-title style="color: white">Real-Time Flood Impact Map</v-toolbar-title>
+      <Geosearch :map="map"></Geosearch>
     </v-app-bar>
-    <Map></Map>
+    <Map @getMap="getChildMap"></Map>
   </v-app>
 </template>
 
@@ -13,10 +14,17 @@
 // imports
 import Map from "./components/Map";
 import Sidebar from "./components/Sidebar";
+import Geosearch from '@/components/Geosearch';
 export default {
   components: {
     Sidebar,
     Map,
+    Geosearch,
+  },
+  data: function () {
+        return {
+            map: ''
+        }
   },
   computed: {
     drawerState: {
@@ -24,5 +32,10 @@ export default {
       set (v) { return this.$store.commit('toggleDrawerState', v) }
     },
   },
+  methods: {
+    getChildMap(mapObject){
+      this.map = mapObject;
+    }
+  }
 };
 </script>
