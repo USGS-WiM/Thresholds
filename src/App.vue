445af3836dc5982a291d2886f380f181dd5bbe52
lilyhoutman <lhoutman@wisc.edu>
2021-07-28 10:00:47 -0500

    add usgs logo to title

--- src/App.vue
+++ src/App.vue
@@ -6,9 +6,10 @@
         @click="drawerState = !drawerState"
         style="color: white"
       ></v-app-bar-nav-icon>
-      <v-toolbar-title style="color: white"
-        >Real-Time Flood Impact Map</v-toolbar-title
-      >
+      <v-toolbar-title style="color: white">
+        <div class="toolbar-image"><img alt="USGS logo" height="60px" src="../public/usgs.png"></div>
+        <div class="toolbar-text"><span>Real-Time Flood Impact Map</span></div>
+      </v-toolbar-title>
       <Geosearch :map="map"></Geosearch>
     </v-app-bar>
     <Map @getMap="getChildMap"></Map>
@@ -64,5 +65,20 @@ html, body {
 
 #app {
   font-family: 'Public Sans', sans-serif !important;
+}
+
+.toolbar-image {
+  display: inline-block;
+  float: left;
+  margin-top: 10px;
+}
+
+.toolbar-text {
+  display: inline-block;
+  line-height: 64px;
+  float: left;
+  margin-top: 8px;
+  padding-left: 20px;
+  font-size: 25px;
 }
 </style>
