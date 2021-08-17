501dbc829e111ed4f8ceebaa0e0541b35837c4c0
lilyhoutman <lhoutman@wisc.edu>
2021-08-17 12:45:23 -0500

    finalize loading for NFHL

--- src/components/Map.vue
+++ src/components/Map.vue
@@ -1041,7 +1041,7 @@ export default {
           self.nfhlIsDisplayed = "none";
           clearInterval(fadeOut);
         }
-      }, 150);
+      }, 100);
     },
     loadAQdata() {
       this.aqMarkers.clearLayers();
@@ -1418,12 +1418,16 @@ export default {
     currentBounds: function () {
       this.streamgageMarkers.clearLayers();
       this.toggleStreamgage(this.streamgageMarkers, this.currentZoom);
+      this.nfhlLayer.remove();
+      this.getNfhlLayer();
     },
     currentZoom: function () {
       // Update legend on zoom
       if (this.map.hasLayer(this.nfhlLayer) && this.nfhlVisible) {
         let layers = this.nfhlLayer.getLayers();
         this.getNfhlLegend(layers);
+        //this.nfhlLayer.remove();
+        //this.getNfhlLayer();
       }
     },
     "$store.state.streamgageState": function () {
